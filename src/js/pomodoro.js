8e8af2382b736a6f5062e9624c23d0b8c4a2ba50
Khoerul Umam <khoerul.umam@gotoko.co.id>
2022-08-07 12:45:56 +0700

    update: pomodoro

--- src/js/pomodoro.js
+++ src/js/pomodoro.js
@@ -1,8 +1,8 @@
 const pomodoroTime = 25;
 const pomodoroShortRest = 5;
 const pomodoroLongRest = 25;
-const finish = 0;
-const step = [pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroLongRest, finish]
+const finish = 'stop';
+let step = [pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroLongRest, finish]
 let currentStep = 0
 let counter = 59;
 let start = false;
@@ -26,24 +26,36 @@ function draw() {
   clear()
   background("#D9622B");
   if (start) {
-    if (counter == 0) {
-      step[currentStep]--;
-      counter = 59
-      currentStep++;
-      alarm.play();
-      start = false;
+    if (step[currentStep] == 'stop') {
+      step = [pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroShortRest, pomodoroTime, pomodoroLongRest, finish]
+      currentStep = 0;
     }
+
     fill('#ffffff');
-    textSize(100)
+    textSize(100);
     textAlign(CENTER, CENTER)
     text(`${step[currentStep] < 10 ? '0' + step[currentStep] : step[currentStep]}:${counter < 10 ? '0' + counter : counter}`, width / 2, height / 2);
     textSize(16);
     if (currentStep % 2 != 0) {
       text(`Current step : ${currentStep + 1} (rest)`, width / 2, 180);
     } else {
-      text(`Current step : ${currentStep + 1} (start)`, width / 2, 180);
+      text(`Current step : ${currentStep + 1} (work)`, width / 2, 180);
     }
-    counter--;
+
+    if (step[currentStep] == 0 && counter == 0) {
+      alarm.play();
+      currentStep++;
+      counter = 59;
+      start = false;
+    }  else {
+      if (counter == 0) {
+        step[currentStep]--;
+        counter = 59;
+      } else {
+        counter--;
+      }
+    }
+
   } else {
     fill('#ffffff');
     textAlign(CENTER, CENTER);
